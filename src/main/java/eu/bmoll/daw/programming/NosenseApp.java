47c8a4a3d0880e24e3c089d7aebb95167016a555
Pamela Mejia Fonseca <pmejia@cifpfbmoll.eu>
2021-02-20 01:04:38 +0100

    Avanzando en las clases

--- src/main/java/eu/bmoll/daw/programming/NosenseApp.java
+++ src/main/java/eu/bmoll/daw/programming/NosenseApp.java
@@ -5,13 +5,23 @@ import org.apache.commons.lang3.StringUtils;
 import java.util.Scanner;
 
 public class NosenseApp {
-    public static void main(String[] args) {
-        boolean aux = StringUtils.equals("a", "A");
-        System.out.println("hola mundo:" + aux);
-        Scanner in = new Scanner(System.in);
+    private static final String EXIT_KEY = "9"
 
-        String s = in.nextLine();
-        System.out.println("You entered string " + s);
+    public static void main(String[] args) {
+        //boolean aux = StringUtils.equals("a", "A");
+       // System.out.println("hola mundo:" + aux);
+       // Scanner in = new Scanner(System.in);
+        ConsoleUtils consoleUtils = new ConsoleUtils();
+        boolean isExit = false;
+        while (isExit){
+            //System.out.println("Introduce una option: ");
+           // String s = in.nextLine();
+            String s = consoleUtils.getUserInput();
+            System.out.println("You entered string " + s);
+            isExit = StringUtils.equals(s, "EXIT_KEY")
+        }
+
+
         System.out.println("-END-");
     }
 
